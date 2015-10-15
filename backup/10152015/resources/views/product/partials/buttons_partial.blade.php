235816acf325230126e861363088231b3ed6502d
root <root@tsmstsrvappdev.(none)>
2015-10-15 18:05:30 +0800

    Remove created backup files

--- backup/10152015/resources/views/product/partials/buttons_partial.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="text-right">
-	<div class="control-btn" id="submit-buttons">
-		<a href="#" class="btn btn-primary btn-submit" data-value="save_exit">Save and Exit</a>
-		<a href="#" class="btn btn-primary btn-submit" data-value="save_keep_editing">Save and Keep Editing</a>
-	</div>
-	<a href="{{ url('product') }}" class="btn btn-default navbar-btn">Cancel</a>
-</div>
--- backup/10152015/resources/views/shared/_alert.blade.php
+++
@@ -1,7 +0,0 @@
-<div class="row">
-	<div class="col-md-12">
-		<div id="top-alert" class="alert" role="alert" style="display:none">
-			<span id="icon" class="glyphicon"></span> <span id="message"></span>
-		</div>
-	</div>
-</div>
